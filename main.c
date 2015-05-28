9e96695e3ffa4f775ffc1834574b63702a3375bb
Guillaume BADI <gbadi@e3r8p7.42.fr>
2015-05-28 18:59:54 +0200

    init

---
+++ main.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sjulliot <sjulliot@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/05/28 18:25:53 by sjulliot          #+#    #+#             */
+/*   Updated: 2015/05/28 18:55:55 by gbadi            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <termcap.h>
+#include <termios.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "libft/libft.h"
+
+struct termios		*start_termios(void)
+{
+	struct termios	tios;
+	struct termios	*orig_tios;    /* terminal settings */
+
+	orig_tios = (struct termios *)malloc(sizeof(struct termios));
+	/* Get current terminal settings */
+	if (tcgetattr(0, orig_tios)){
+		ft_putendl("Error getting current terminal settingsn");
+		return (NULL);
+	}
+
+	/* Copy that to "tios" and play with it */
+	tios = *orig_tios;
+	tios.c_lflag &= ~ICANON;
+	tios.c_lflag |= ECHO;
+	if (tcsetattr(0, TCSANOW, &tios)){
+		ft_putendl("Error applying terminal settingsn");
+		return (NULL);
+	}
+	/* Check whether our settings were correctly applied */
+	if (tcgetattr(0, &tios)){
+		tcsetattr(0, TCSANOW, orig_tios);
+		ft_putendl("Error while asserting terminal settingsn");
+		return (NULL);
+	}
+	if ((tios.c_lflag & ICANON) || !(tios.c_lflag & ECHO)) {
+		tcsetattr(0, TCSANOW, orig_tios);
+		ft_putendl("Could not apply all terminal settingsn");
+		return (NULL);
+	}
+	return (orig_tios);
+}
+
+void				end_termios(struct termios *orig_tios)
+{
+	tcsetattr(0, TCSANOW, orig_tios);
+	free(orig_tios);
+}
+
+int					main(int ac, char **av)
+{
+	struct termios	*term_conf_save;
+
+	(void)ac;
+	(void)av;
+	term_conf_save = start_termios();
+	end_termios(term_conf_save);
+	return (0);
+}
